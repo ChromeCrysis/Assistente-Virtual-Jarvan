702a800a12c07bbf5c136051a36f597842086d4e
ChromeCrysis <anderson.bechelli@gmail.com>
2020-01-13 02:35:41 -0300

    correção de erros na síntaxe do método de instalação das dependências

--- src/dependencias/dependencies.py
+++ src/dependencias/dependencies.py
@@ -6,10 +6,17 @@ print("Olá, sou o instalador de dependencias do Jarvan. Para funcionar, Jarvan 
 print("Essas dependências são: \nPyAudio.\nSpeechRecognition.\nPyttsx3.")
 print("Deseja instalar as dependências?")
 confirmacao = str(input('Digite S para Sim e N para Não:'))
-if confirmacao == N:
-    print("Entendido, não será instalado nada.") 
-    #exit()
+
+if confirmacao == 'S' or confirmacao == 's':
+    if os.name == 'nt':
+        os.system("pip install PyAudio")
+        os.system("pip install SpeechRecognition")
+        os.system("pip install pyttsx3")
+    else:
+        os.system("sudo pip install PyAudio")
+        os.system("sudo pip install SpeechRecognition")
+        os.system("sudo pip install pyttsx3")
 else:
-    os.system("pip install PyAudio")
-    os.system("pip install SpeechRecognition")
-    os.system("pip install pyttsx3")
+    print("Entendido, não será instalado nada.") 
+    exit()
+    
